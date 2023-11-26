8e396c2bd7575c65dc613c66ad2cc4984dca6900
Megha Saji <meghasaji2002@gmail.com>
2023-11-26 15:03:06 +0530

    simple react design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,27 +11,29 @@ function Navbar() {
 <>
 
 
-     <div style={{width:'100%',height:'80px',padding:'0px 10px', display:'flex',alignItems:'center',backgroundColor:'black'}} >
+     <div  style={{width:'100%',height:'80px',padding:'0px 10px', display:'flex',alignItems:'center',backgroundColor:'black'}} >
           <div>
           
           <img style={{height:'60px'}} src={primevideotitle} alt="" />
           </div>
-          <h6 style={{color:'white',marginLeft:'20px'}}>Home</h6>
-          <h6 style={{color:'white',marginLeft:'15px'}}>TV Shows</h6>
-         <h6 style={{color:'white',marginLeft:'15px'}}>Movies</h6>
-          <h6 style={{color:'white',marginLeft:'15px'}}>Kids</h6>
+         <div className='w-25' style={{display:'flex',alignContent:'center',justifyContent:'center'}}>
+            <h6 style={{color:'white',flexGrow:'1'}}>Home</h6>
+            <h6 style={{color:'white',flexGrow:'1'}}>TV Shows</h6>
+           <h6 style={{color:'white',flexGrow:'1'}}>Movies</h6>
+            <h6 style={{color:'white',flexGrow:'1'}}>Kids</h6>
+         </div>
   
   
         
-        <div className='ms-auto' style={{display:'flex',alignItems:'center',justifyContent:'center'}}>
-        <div  style={{display:'flex',alignItems:'center',justifyContent:'center'}}> 
+        <div  className='ms-auto' style={{display:'flex',alignItems:'center',justifyContent:'center'}}>
+        <div  style={{display:'flex',flexGrow:'2' ,alignItems:'center',justifyContent:'center'}}> 
          <div style={{border:'1px solid white',borderRadius:'5px'}}  ><i style={{color:'gray',fontSize:'18px'}} class="fa-solid fa-magnifying-glass ms-2 "></i><input className=' rounded ps-2 ms-2  ' style={{height:'30px',background:'transparent',border:'0'}}  placeholder='Search' type="text" /></div>
            </div>
          
   
-           <div style={{height:'35px',width:'35px',borderRadius:'50%',border:'1px solid white',display:'flex',alignItems:'center',justifyContent:'center',marginLeft:'10px'}}><i class="fa-solid fa-user" style={{color:'gray',fontSize:'20px'}}></i></div>
+           <div style={{height:'35px',width:'35px',borderRadius:'50%',border:'1px solid white',display:'flex',alignItems:'center',justifyContent:'center',marginLeft:'10px',flexGrow:'1' }}><i class="fa-solid fa-user" style={{color:'gray',fontSize:'20px'}}></i></div>
   
-           <NavDropdown className='ms-2 ' style={{color:'white'}} title="Profile" id="basic-nav-dropdown">
+           <NavDropdown className='ms-2 ' style={{color:'white',flexGrow:'1' }} title="Profile" id="basic-nav-dropdown">
                 
                 <NavDropdown.Item className='text-center' href="#action/3.1">Plans </NavDropdown.Item>
                 <NavDropdown.Item className='text-center' href="#action/3.2">
